83685e46a72dfda037367b18ef1397b2cb640869
gbischof <bisc8233@gmail.com>
2023-04-21 11:32:17 -0400

    remove some files

--- example.py
+++
@@ -1,8 +0,0 @@
-from tiled.client import from_profile 
-from tiled.queries import Key
-
-tiled_client = from_profile("staging", "dask", username=None)["chx"]
-tiled_client_sandbox = tiled_client["sandbox"]
-results = tiled_client_sandbox.search(Key("spec") == "mask")
-vals = results.values()
-vals.first()
