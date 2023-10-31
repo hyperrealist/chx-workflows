791207643106a826046d722bf927cc5216f3c8b4
Juan Marulanda Arias <jmaruland@bnl.gov>
2023-10-31 14:35:14 -0400

    Add template processing flow

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,5 +1,6 @@
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
+from processing import processing_flow
 
 
 @task
@@ -11,5 +12,7 @@ def log_completion():
 @flow
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
-    data_validation(uid)
+    # return_state = True delays raising exceptions until the end of the validation
+    data_validation(uid, return_state=True)
+    processing_flow(uid)
     log_completion()
